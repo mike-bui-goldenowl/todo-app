50c3382e26f0c49e0ad50f91fdcdde2a0c401bb1
Hoang21099 <hoang21099@gmail.com>
2020-02-10 14:40:45 +0700

    fix close function in modal component

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -4,6 +4,7 @@ import {
   useHistory,
   useRouteMatch,
 } from 'react-router-dom';
+import { AiFillCloseSquare } from 'react-icons/ai';
 
 // Selector
 import { selectTodoList } from '../../redux/todo/todo.selector';
@@ -24,19 +25,29 @@ export default connect(mapStateToProps)(({ todoList }) => {
   };
   const item = todoList.filter((todoItem) => todoItem.id.toString() === match.params.id)[0];
   return (
-    <div
-      role="button"
-      tabIndex={0}
-      onClick={back}
-      onKeyDown={back}
-      className="content-modal"
-    >
-      <h3>
-        Todo Item -
-        {' '}
-        {item.isCompleted ? 'Completed' : ' In Process'}
-      </h3>
-      <TodoItem item={item} />
+    <div className="block-modal">
+      <div
+        className="content-modal"
+      >
+        <div className="top">
+          <h3>
+            Todo Item -
+            {' '}
+            {item.isCompleted ? 'Completed' : ' In Process'}
+          </h3>
+          <AiFillCloseSquare
+            size={35}
+            className="button-close"
+            role="button"
+            tabIndex={0}
+            onClick={back}
+            onKeyDown={back}
+          />
+
+        </div>
+        <TodoItem item={item} />
+      </div>
     </div>
+
   );
 });
